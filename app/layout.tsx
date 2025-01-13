97643fda5466c74ae4ce78647a5e1556684be169
Okorie Emmanuel <okorieemmanuel167@gmail.com>
2025-01-13 18:46:44 +0100

    init

---
+++ app/layout.tsx
@@ -0,0 +1,58 @@
+"use client";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "./provider";
+import { useEffect, useState } from "react";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const metadata: Metadata = {
+  title: "Okorie Emmanuel's Portfolio",
+  description:
+    "Welcome to Okorie Emmanuel's Portfolio - Showcasing innovative Mobile and Web frontend development, and creative projects.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const [hydrated, setHydrated] = useState(false);
+
+  useEffect(() => {
+    // Ensures the component is rendered only after hydration
+    setHydrated(true);
+  }, []);
+
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <div>
+          {hydrated ? (
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="dark"
+              enableSystem
+              disableTransitionOnChange
+            >
+              {children}
+            </ThemeProvider>
+          ) : (
+            children // Render children without theme provider during SSR
+          )}
+        </div>
+      </body>
+    </html>
+  );
+}
